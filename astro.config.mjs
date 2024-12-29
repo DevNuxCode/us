0901c58fd450ecc9e50b24c5cd07d50c7464fc0b
DevNuxCode <devnuxcode@gmail.com>
2024-12-29 20:31:06 -0300

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import alpinejs from '@astrojs/alpinejs';
 
 export default defineConfig({
-  integrations: [tailwind(), react(), alpinejs()],
+  integrations: [tailwind(), react()],
 });
