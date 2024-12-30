dce4d545e80b86343753cd4eca07f14d77177067
DevNuxCode <devnuxcode@gmail.com>
2024-12-30 02:59:05 -0300

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'static', // o 'server' dependiendo de tu caso
-  adapter: '@astrojs/vercel'
+  //adapter: '@astrojs/vercel'
 });
