cd506662e903d6b57162652a82a53149b17ee3a4
DevNuxCode <devnuxcode@gmail.com>
2024-12-30 02:39:47 -0300

    Update PC

---
+++ src/utils/api.ts
@@ -0,0 +1,27 @@
+export async function getVideos() {
+  const response = await fetch('https://www.python.distribuidoravega.cl/py/wp-json/wp/v2/video');
+  const videos = await response.json();
+  
+  return Promise.all(
+    videos.map(async (video: any) => {
+      let imageUrl = '';
+      if (video._links['wp:featuredmedia']) {
+        const mediaId = video._links['wp:featuredmedia'][0].href.split('/').pop();
+        const mediaResponse = await fetch(`https://www.python.distribuidoravega.cl/py/wp-json/wp/v2/media/${mediaId}`);
+        const mediaData = await mediaResponse.json();
+        imageUrl = mediaData.guid.rendered;
+      }
+      
+      return {
+        id: video.id,
+        date: video.date,
+        content: video.content.rendered,
+        imageUrl,
+        name: video.acf?.name || '',
+        note: video.acf?.note || '',
+        bpm: video.acf?.bpm || '',
+        url: video.acf?.url || ''
+      };
+    })
+  );
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,30 @@
+export interface Video {
+  id: number;
+  date: string;
+  content: {
+    rendered: string;
+  };
+  _links: {
+    'wp:featuredmedia': Array<{
+      href: string;
+    }>;
+  };
+  acf: {
+    name?: string;
+    note?: string;
+    bpm?: string;
+    url?: string;
+  };
+  imageUrl?: string;
+}
+
+export interface VideoCardProps {
+  id: number;
+  date: string;
+  content: string;
+  imageUrl: string;
+  name?: string;
+  note?: string;
+  bpm?: string;
+  url?: string;
+}
